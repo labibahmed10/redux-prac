0c487934a9206847c19b847b6645a92e370f9487
labibahmed10 <labib.ahmed.372@gmail.com>
2022-12-26 21:45:14 +0600

    fixed the border of header

--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -8,14 +8,14 @@ const HeroSection = () => {
             <img src={heroImage} alt="" />
          </div>
 
-         <aside className="bg-[#047267] lg:py-10 py-3 flex justify-center items-center lg:gap-10 gap-2">
-            <div className="text-white">
+         <aside className="bg-[#047267] lg:py-10 py-3 flex justify-center items-center lg:gap-20 gap-2">
+            <div className="text-white lg:border-r-8 lg:border-r-white lg:pr-3">
                <h1 className="lg:text-[37px] text-[12px] font-bold">
                   <span className="lg:text-[50px] text-[16px]">01</span>+ Million
                </h1>
                <p className="text-[12px] font-[500]">Tickets Sold</p>
             </div>
-            <div className="text-white">
+            <div className="text-white lg:border-r-8 lg:border-r-white lg:pr-10">
                <h1 className="lg:text-[50px] text-[16px] font-bold">100</h1>
                <p>Routes</p>
             </div>
