83833573ef70836df47191de53de04f302b8d2ab
labibahmed10 <labib.ahmed.372@gmail.com>
2022-12-27 12:38:33 +0600

    starting over

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
-import HeroSection from "./component/HeroSection";
-import IntroductionPart from "./component/introPart/IntroductionPart";
-import Navbar from "./component/Navbar";
-import ServicesPart from "./component/service/ServicesPart";
-
 function App() {
-   return (
-      <div className="bg-[#f5f5f5]">
-         <Navbar />
-         <HeroSection />
-         <ServicesPart />
-         <IntroductionPart />
-      </div>
-   );
+   return <div></div>;
 }
 
 export default App;
--- src/component/Navbar.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Navbar = () => {
-   return <div className="lg:py-16 py-5"></div>;
-};
-
-export default Navbar;
